8cf61d52c0602e1dc75bd26235e98028eca863f9
Yoann ESSENGUE-DOOH <103749193+yoannDooh@users.noreply.github.com>
2023-11-04 21:33:25 +0100

    exo 2 almost finished

--- exercices/exo2/src/Main.cpp
+++ exercices/exo2/src/Main.cpp
@@ -1,6 +1,7 @@
 #include "../header/shader.h"
 #include "../header/window.h"
 #include <cmath>
+#include <chrono>
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
@@ -13,8 +14,8 @@ const unsigned int SCR_HEIGHT = 600;
 const float vertices[]{
                         //coordinates           //colors
                         0.0f, 0.5f, 0.0f,    1.0f, 0.0f, 0.0f,       // top 
-                        -0.5f, -0.5f, 0.0f,   1.0f, 1.0f, 0.0f,        // bottom right
-                        0.5f, -0.5f, 0.0f,  0.0f, 0.0f, 1.0f,       // bottom left
+                        -0.5f, -0.5f, 0.0f,   1.0f, 1.0f, 0.0f,        // bottom left
+                        0.5f, -0.5f, 0.0f,  0.0f, 0.0f, 1.0f,       // bottom right
                     };
 
 unsigned int indices[]  { 
@@ -49,69 +50,112 @@ int main()
     glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)( 3*sizeof(float)) );
     glEnableVertexAttribArray(1);
 
-    //setup rotation matrix 
-    glm::mat4 trans = glm::mat4(1.0f);
-    trans = glm::rotate(trans, glm::radians(180.0f), glm::vec3(1.0f, 0.0f, 0.0f));
 
-   
-    // render loop
+
+    //trans = glm::rotate(trans, glm::radians(360.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+
+     //setup rotation matrix 
+    glm::mat4 model{ glm::mat4(1.0f) };
+    float bottomLeftCoord{ -0.5 };
+    float fps{ 60 };
+    float transPerFrame{ ( (1.0f-bottomLeftCoord)/2.0f) /fps }; 
     bool isItFirstLoop{ true };
-    while (!glfwWindowShouldClose(window.windowPtr))
-    {
-        if (isItFirstLoop)
-        { 
-            shader.setFloat("U_offset", 0.5f);
-            processInput(window.windowPtr);
-            glfwSetTime(0);
+   
 
-            while (glfwGetTime() <= 2)
-            {
+ // render loop
+ while (!glfwWindowShouldClose(window.windowPtr))
+ {
+    if (isItFirstLoop)
+    {   
+        int currentSec{ 0 };
+        int count{ 1 };
 
-                shader.setFloat("U_offset2", glfwGetTime());
-               
-                glClearColor(107.0f / 255.0f, 142.0f / 255.0f, 35.0f / 255.0f, 1.0f);
-                glClear(GL_COLOR_BUFFER_BIT);
-                glUniformMatrix4fv(glGetUniformLocation(shader.ID, "trans"), 1, GL_FALSE, glm::value_ptr(trans));
-                
-                shader.use();
-
-                //activate program object 
-                glBindVertexArray(VAO);
-                glDrawArrays(GL_TRIANGLES, 0, 3);
-                glBindVertexArray(0);
-                glfwSwapBuffers(window.windowPtr);
-                glfwPollEvents();
-                isItFirstLoop =false;
-            }
-        }
-
-        else
+        while (currentSec < 2)
         {
-            shader.setFloat("U_offset", 1.5f);
-            processInput(window.windowPtr);
+            int currentFrame{1};
             glfwSetTime(0);
-            while (glfwGetTime() <= 3)
+            double t1{ glfwGetTime() };
+            std::cerr << 1 / fps;
+            while (currentFrame <= 60)
             {
-                //glUniform1f(glGetUniformLocation(shader.ID, "U_angle"), glfwGetTime());
-                shader.setFloat("U_offset2", glfwGetTime());
-                glClearColor(107.0f / 255.0f, 142.0f / 255.0f, 35.0f / 255.0f, 1.0f);
-                glClear(GL_COLOR_BUFFER_BIT);
-                glUniformMatrix4fv(glGetUniformLocation(shader.ID, "trans"), 1, GL_FALSE, glm::value_ptr(trans));
+                if (glfwGetTime() >= t1 + 1/fps)
+                {  
+                    //translation matrix
+                    model = glm::translate(model, glm::vec3(transPerFrame, 0.0f, 0.0f));
 
-                shader.use();
+                    //rotation matrix
+                    //model = glm::rotate(model, glm::radians(1.5f), glm::vec3(0.0f, 0.0f, 1.0f));
 
-                //activate program object 
-                glBindVertexArray(VAO);
-                glDrawArrays(GL_TRIANGLES, 0, 3);
-                glBindVertexArray(0);
-                glfwSwapBuffers(window.windowPtr);
-                glfwPollEvents();
-                isItFirstLoop = false;
+                    glUniformMatrix4fv(glGetUniformLocation(shader.ID, "model"), 1, GL_FALSE, glm::value_ptr(model));
+                    
+                    //background color
+                    glClearColor(107.0f / 255.0f, 142.0f / 255.0f, 35.0f / 255.0f, 1.0f);
+                    glClear(GL_COLOR_BUFFER_BIT);
+                    shader.use();
+
+                    //activate program object 
+                    glBindVertexArray(VAO);
+                    glDrawArrays(GL_TRIANGLES, 0, 3);
+                    glBindVertexArray(0);
+                    glfwSwapBuffers(window.windowPtr);
+                    glfwPollEvents();
+
+
+                    ++currentFrame;
+                    std::cout << count << '\n';
+                    ++count;
+                    t1 = glfwGetTime();
+                 }    
+             }  
+            ++currentSec;
+         }
+        isItFirstLoop = false;
+     }
+
+     else
+     {
+        model = glm::translate(model, glm::vec3(-3, 0.0f, 0.0f));
+        glUniformMatrix4fv(glGetUniformLocation(shader.ID, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        int currentSec{ 0 };
+        int count{ 1 };
+        while (currentSec < 4)
+         {
+            int currentFrame{ 1 };
+            glfwSetTime(0);
+            double t1{ glfwGetTime() };
+            while (currentFrame <= 60)
+            {
+                if (glfwGetTime() >= t1 + 1 / fps)
+                {
+                    //translation matrix
+                    model = glm::translate(model, glm::vec3(transPerFrame, 0.0f, 0.0f));
+                    glUniformMatrix4fv(glGetUniformLocation(shader.ID, "model"), 1, GL_FALSE, glm::value_ptr(model));
+
+                    //background color
+                    glClearColor(107.0f / 255.0f, 142.0f / 255.0f, 35.0f / 255.0f, 1.0f);
+                    glClear(GL_COLOR_BUFFER_BIT);
+                    shader.use();
+
+                    //activate program object 
+                    glBindVertexArray(VAO);
+                    glDrawArrays(GL_TRIANGLES, 0, 3);
+                    glBindVertexArray(0);
+                    glfwSwapBuffers(window.windowPtr);
+                    glfwPollEvents();
+
+                    ++currentFrame;
+                    std::cout << count << '\n';
+                    ++count;
+                    t1 = glfwGetTime();
+                }
             }
-        }   
-    }
-    processInput(window.windowPtr);
-    glfwTerminate();
+
+            ++currentSec;
+         }
+     }   
+ }
+
+ glfwTerminate();
     return 0;
 }
 
